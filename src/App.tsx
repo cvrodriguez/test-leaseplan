db453c46ca11c3b2f26c1f9bc127aa05be4af32f
Viviana Rodriguez <cinvir15@gmail.com>
2018-08-11 21:37:56 +0200

    create project and Dashboard page

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+import * as React from 'react';
+import './App.css';
+
+import {   } from 'antd';
+import 'antd/dist/antd.css';
+
+
+import logo from './logo.svg';
+
+
+
+class App extends React.Component {
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome Viviana's World</h1>
+        </header>
+        <p className="App-intro" >
+         <strong> I'm Viviana </strong> 
+         this is my test, I don't have experience,
+          but I'm happy for this change
+        
+        </p>
+        <a> Go to email</a>
+        <br/>
+        <a> Go to repository</a>
+      </div>
+    );
+  }
+}
+
+export default App;
